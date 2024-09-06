f5cb10d9ec285525f196fa19b0769a1ecb5aa838
Amlan Kumar <amlantest23@example.com>
2024-09-06 20:49:38 +0530

    6th Sept

--- src/test/java/com/amlankumar/base/CommonToAllTests.java
+++ src/test/java/com/amlankumar/base/CommonToAllTests.java
@@ -2,7 +2,9 @@ package com.amlankumar.base;
 
 import com.amlankumar.drivers.DriverManager;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class CommonToAllTests {
 
--- src/test/java/com/amlankumar/tests/TestVWOLogin_PF_DM.java
+++ src/test/java/com/amlankumar/tests/TestVWOLogin_PF_DM.java
@@ -3,6 +3,9 @@ package com.amlankumar.tests;
 import com.amlankumar.base.CommonToAllTests;
 import com.amlankumar.pages.pageFactory.Login_PF;
 import com.amlankumar.utils.PropertyReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
@@ -14,11 +17,19 @@ import static com.amlankumar.drivers.DriverManager.driver;
 
 public class TestVWOLogin_PF_DM extends CommonToAllTests {
 
+    private static final Logger logger = LogManager.getLogger(TestVWOLogin_withLogs.class);
+
     @Test
     public void VWONegative() throws FileNotFoundException {
+
         Login_PF loginPf = new Login_PF(driver);
+        logger.info("Starting test -- Opening URL");
         loginPf.openVWOLoginURL("qa");
+        logger.info("Logging in");
         String error_msg_text = loginPf.loginToVWOInvalidCreds();
+        logger.info("Verifying result");
         Assert.assertEquals(error_msg_text, "Your email, password, IP address or location did not match");
+        Assertions.assertThat(error_msg_text).isNotNull().isNotBlank();
     }
+
 }
